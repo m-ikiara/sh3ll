5546a9018da2bcf05872ca358178caa5589341ed
brian-ikiara <brnikiara@gmail.com>
2022-12-19 10:47:48 +0000

    Create shell.h

---
+++ shell.h
@@ -0,0 +1,18 @@
+#ifndef SIMPLE_H
+#define SIMPLE_H
+
+#define _GNU_SOURCE
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <sys/wait.h>
+#include <stddef.h>
+#include <signal.h>
+#include <string.h>
+#include <errno.h>
+
+#endif
